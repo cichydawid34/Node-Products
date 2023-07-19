d70e8d97651334300357b68bd7dd30304f1ee3c7
Dawid Cichy <dawidcichy34@gmail.com>
2023-07-19 07:57:06 +0200

    XDD

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.use("/users", routerUsers);
 
 //Deployment
 __dirname = path.resolve();
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV == "XDA") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
